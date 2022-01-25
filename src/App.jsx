eb1e6a13c3aa5948f64ba23b7984b89c0df1f655
finn-01 <nguyensam576@gmail.com>
2022-01-25 17:11:43 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,22 @@ const App = () => {
 						</Routes>
 					</div>
 				</Layout>
-			</div>
 
-			<div className="footer"></div>
+				<div className="footer">
+					<Typography.Title
+						level={5}
+						style={{ color: "white", textAlign: "center" }}
+					>
+						Crytoverse <br />
+						All rights reserved
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
+			</div>
 		</div>
 	);
 };
